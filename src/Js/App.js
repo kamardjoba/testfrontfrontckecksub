e0b99dde1f00457969005ee586c6731ca6cfeeda
kamardjoba <Nazarlymar152@gmail.com>
2024-07-21 03:17:38 +0200

    initial

--- src/Js/App.js
+++ src/Js/App.js
@@ -93,6 +93,27 @@ function App() {
       const response = await axios.post(`${REACT_APP_BACKEND_URL}/check-subscription-and-update`, { userId });
       const data = response.data;
       if (response.status === 200) {
+
+        if (data.hasCheckedSubscription) {
+          localStorage.setItem('Galka', 'true');
+          localStorage.setItem('Knopka', 'false');
+          setSubscriptionCoins(1000);
+        } else {
+          localStorage.setItem('Galka', 'false');
+          localStorage.setItem('Knopka', 'true');
+          setSubscriptionCoins(0);
+        }
+  
+        if (data.hasCheckedSubscription2) {
+          localStorage.setItem('Galka2', 'true');
+          localStorage.setItem('Knopka2', 'false');
+          setSubscriptionCoins2(750);
+        } else {
+          localStorage.setItem('Galka2', 'false');
+          localStorage.setItem('Knopka2', 'true');
+          setSubscriptionCoins2(0);
+        }
+        
         setCoins(response.data.coins);
       } else {
         console.error('Ошибка при проверке подписки:', response.data.message);
@@ -236,7 +257,7 @@ function App() {
       console.error('Ошибка при проверке подписки:', error);
     }
   };
-  
+
   
 
   useEffect(() => {
