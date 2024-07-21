01ab1195bfa12d0364a9287bd6c84d259536b481
kamardjoba <Nazarlymar152@gmail.com>
2024-07-21 05:36:36 +0200

    initial

--- src/Js/App.js
+++ src/Js/App.js
@@ -234,11 +234,16 @@ function App() {
       if (response.status === 200) {
         const data = response.data;
         setCoins(data.coins);
-        //setSubscriptionCoins(response.data.subscriptions ? 1000 : 0);
-        //setSubscriptionCoins2(response.data.subscriptions2 ? 750 : 0);
 
-     
-
+        if (data.hasCheckedSubscription) {
+          localStorage.setItem('Galka', 'true');
+          localStorage.setItem('Knopka', 'false');
+          setSubscriptionCoins(1000);
+        } else {
+          localStorage.setItem('Galka', 'false');
+          localStorage.setItem('Knopka', 'true');
+          setSubscriptionCoins(0);
+        }
       } else {
         console.error('Ошибка при проверке подписки:', response.data.error);
       }
