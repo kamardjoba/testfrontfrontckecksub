98ae7b3835ebcb5920ad623b5615b03ae1bac49e
kamardjoba <Nazarlymar152@gmail.com>
2024-07-21 05:55:22 +0200

    initial

--- src/Js/App.js
+++ src/Js/App.js
@@ -143,18 +143,6 @@ function App() {
         const data = response.data;
         setCoins(data.coins);
 
-        if (data.hasCheckedSubscription2) {
-          localStorage.setItem('Galka2', 'true');
-          localStorage.setItem('Knopka2', 'false');
-          setSubscriptionCoins2(750);
-        } else {
-          localStorage.setItem('Galka2', 'false');
-          localStorage.setItem('Knopka2', 'true');
-          setSubscriptionCoins2(0);
-        }
-
-       
-
       } else {
         console.error('Ошибка при проверке подписки:', response.data.message);
       }
@@ -189,7 +177,6 @@ function App() {
         setYearr(yearsOld);
         const accountAgeCoins = yearsOld * 500;
         setcoinOnlyYears(accountAgeCoins);
-
        
         if (hasTelegramPremium === true) {
           setVisibleTelegramPremium(true);
@@ -218,7 +205,6 @@ function App() {
           localStorage.setItem('Knopka', 'true');
           setSubscriptionCoins(0);
         }
-
   
         setAccountAgeCoins(accountAgeCoins);
   
