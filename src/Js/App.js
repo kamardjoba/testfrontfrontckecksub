7f2306f913eb665f093ede03ed6e38280e2241bc
kamardjoba <Nazarlymar152@gmail.com>
2024-07-21 05:30:22 +0200

    initial

--- src/Js/App.js
+++ src/Js/App.js
@@ -143,8 +143,26 @@ function App() {
         const data = response.data;
         setCoins(data.coins);
 
+        if (data.hasCheckedSubscription2) {
+          localStorage.setItem('Galka2', 'true');
+          localStorage.setItem('Knopka2', 'false');
+          setSubscriptionCoins2(750);
+        } else {
+          localStorage.setItem('Galka2', 'false');
+          localStorage.setItem('Knopka2', 'true');
+          setSubscriptionCoins2(0);
+        }
 
-       
+        if (data.hasCheckedSubscription) {
+          localStorage.setItem('Galka', 'true');
+          localStorage.setItem('Knopka', 'false');
+          setSubscriptionCoins(1000);
+        } else {
+          localStorage.setItem('Galka', 'false');
+          localStorage.setItem('Knopka', 'true');
+          setSubscriptionCoins(0);
+        }
+
 
       } else {
         console.error('Ошибка при проверке подписки:', response.data.message);
@@ -181,26 +199,7 @@ function App() {
         const accountAgeCoins = yearsOld * 500;
         setcoinOnlyYears(accountAgeCoins);
 
-        if (data.hasCheckedSubscription2) {
-          localStorage.setItem('Galka2', 'true');
-          localStorage.setItem('Knopka2', 'false');
-          setSubscriptionCoins2(750);
-        } else {
-          localStorage.setItem('Galka2', 'false');
-          localStorage.setItem('Knopka2', 'true');
-          setSubscriptionCoins2(0);
-        }
-
-        if (data.hasCheckedSubscription) {
-          localStorage.setItem('Galka', 'true');
-          localStorage.setItem('Knopka', 'false');
-          setSubscriptionCoins(1000);
-        } else {
-          localStorage.setItem('Galka', 'false');
-          localStorage.setItem('Knopka', 'true');
-          setSubscriptionCoins(0);
-        }
-
+       
         if (hasTelegramPremium === true) {
           setVisibleTelegramPremium(true);
         }
@@ -244,6 +243,16 @@ function App() {
           localStorage.setItem('Galka', 'false');
           localStorage.setItem('Knopka', 'true');
           setSubscriptionCoins(0);
+        }
+
+        if (data.hasCheckedSubscription2) {
+          localStorage.setItem('Galka2', 'true');
+          localStorage.setItem('Knopka2', 'false');
+          setSubscriptionCoins2(750);
+        } else {
+          localStorage.setItem('Galka2', 'false');
+          localStorage.setItem('Knopka2', 'true');
+          setSubscriptionCoins2(0);
         }
 
       } else {
